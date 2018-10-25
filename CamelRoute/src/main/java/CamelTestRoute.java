85c68e498af66b17665f14cbcb207a1696d0b30d
Jarl Kirkensgaard Due <jkd@miracle.dk>
2018-10-25 06:28:28 +0200

    First Commit

---
+++ CamelRoute/src/main/java/CamelTestRoute.java
@@ -0,0 +1,56 @@
+import org.apache.camel.builder.RouteBuilder;
+
+
+public class CamelTestRoute extends RouteBuilder {
+
+//<editor-fold desc="Full Route Description">
+    //Define Finals Here
+
+//<editor-fold desc="Direct Log Endpoints">
+
+
+    //-------------------------------------------------------------------------------//
+//</editor-fold>
+
+//<editor-fold desc="Route Start Points">
+
+
+    //-------------------------------------------------------------------------------//
+//</editor-fold>
+
+//<editor-fold desc="Routes By ID">
+
+
+    //-------------------------------------------------------------------------------//
+//</editor-fold>
+
+//<editor-fold desc="Specific Route Endpoints">
+
+
+    //-------------------------------------------------------------------------------//
+//</editor-fold>
+
+//<editor-fold desc="Routes Walkthrough">
+
+   
+      /*
+      Route Walkthrough
+      Purpose
+      Tools
+      Speficis
+      Error Handling
+     */
+    //</editor-fold>
+
+//</editor-fold>
+
+
+    @Override
+    public void configure() throws Exception {
+        
+        from("timer:foo?period=10s").log("message sent").to("mock:test");
+
+    }
+
+
+}
